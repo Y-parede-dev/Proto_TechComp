f0bcef9cf3d9a4d9bff839ad20467b94fb8844c7
Y-parede-dev <parede.yoan.amcs@gmail.com>
2023-08-07 22:05:32 +0200

    supression et commentaire sur des parties inutilisee

--- app/pages/pc-portable/page.js
+++ app/pages/pc-portable/page.js
@@ -1,9 +1,8 @@
 
 import styles from './page.module.css'
-import produits from '@/data/dataProduits/produits.json' assert { type: 'json' }
-import Script from 'next/script'
-import { metadata } from '@/app/layout'
-import Link from 'next/link'
+// import produits from '@/data/dataProduits/produits.json' assert { type: 'json' }
+// import Script from 'next/script'
+// import Link from 'next/link'
 import SuggestionMap from '@/app/components/suggesttion/SuggestionMap'
 const Product = () => {
 
