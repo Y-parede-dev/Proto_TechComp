e06bf8b035797e65373dee518b71ccb4b3d54f7a
Y-parede-dev <parede.yoan.amcs@gmail.com>
2023-08-22 18:55:35 +0200

    rewowrk footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
 
             </div>
             <div className={styles.txtFooter}>
-                <p>2023 - tous droit réservé - {config.title}</p>
+                <p>2023 - Tous droits réservés - {config.title}</p>
                 <p>Trouvé votre futur {config.productType} sur {config.title}</p>
                 <p>{config.title} recherche pour vous les meilleurs choix en therme de {config.productType}</p>
             </div>
