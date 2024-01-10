03bb2a13ad05775fce561e68962a9b6957db5f6f
Y-parede-dev <parede.yoan.amcs@gmail.com>
2024-01-10 23:38:45 +0100

    maj pub leftright

--- app/components/pub/left.jsx
+++ app/components/pub/left.jsx
@@ -1,12 +1,12 @@
 // link https://clk.tradedoubler.com/click?p=320984&a=3342379&g=25082228
 import Image from "next/image";
-import PUBIMG from "@/public/images/publeftrighttroiscentsixcent.jpg";
+import PUBIMG from "@/public/images/leftrightpub.jpg";
 
 export const BannerAdsLeft = () => {
     return(
         <Image 
             src={PUBIMG}
-            width={300}
+            width={160}
             height={600}
             alt="baniere pub left"/>
     )
--- app/components/pub/right.jsx
+++ app/components/pub/right.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import PUBIMG from "@/public/images/publeftrighttroiscentsixcent.jpg";
+import PUBIMG from "@/public/images/leftrightpub.jpg";
 
 export const BannerAdsRight = () => {
     return(
 // link https://clk.tradedoubler.com/click?p=320984&a=3342379&g=25082228
         <Image 
             src={PUBIMG}
-            width={300}
+            width={160}
             height={600}
             alt="baniere pub right"/>
     )
